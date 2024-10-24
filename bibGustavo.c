4be6c293c151273ad535a35d6da71ac7c5ce3651
Gustavo Lopes <136193786+GustavoLopesOliveira@users.noreply.github.com>
2024-10-24 12:01:08 -0300

    Update bibGustavo.c

--- bibGustavo.c
+++ bibGustavo.c
@@ -35,8 +35,8 @@ int *preencherMatriz(int l, int c){
   
   int *array = (int *) malloc(l * c * sizeof(int));
   
-  for(int i = 1; i <= l; i++){
-    for(int j = 1; j <= c; j++){
+  for(int i = 0; i < l; i++){
+    for(int j = 0; j < c; j++){
       scanf("%d", array + i * c + j);
     }
   }
